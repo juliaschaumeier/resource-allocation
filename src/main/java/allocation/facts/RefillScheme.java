192b1548846cc42c647c81d855bcf80db1385943
Julia Schaumeier <julia.schaumeier@gmx.net>
2013-03-24 18:13:35 +0000

    HIGH, MEDIUM, LOW refill schemes added

--- src/main/java/allocation/facts/RefillScheme.java
+++ src/main/java/allocation/facts/RefillScheme.java
@@ -1,7 +1,7 @@
 package allocation.facts;
 
 public enum RefillScheme {
-	CUSTOM, HIGH, LOW, MEMBER, CLUSTERC, CLUSTERH, CLUSTERL
+	CUSTOM, HIGH, MEDIUM, LOW, MEMBER, CLUSTERC, CLUSTERH, CLUSTERM, CLUSTERL
 };
 
 
